2dacde15d0b6573112c1426059d7b6940b9d5723
myohei <yohei.maeda.11@gmail.com>
2022-07-10 00:03:36 +0900

    fix

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="ffmpeg-streaming",
+    packages=find_packages(exclude=('tests*',)),
     version="0.1.16",
     # install_requires=[],
     entry_points={}
